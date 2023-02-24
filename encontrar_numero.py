58c81a2eddf3cbe54638dbb3b3818abf4601750e
Ubuntu <ubuntu@ip-172-31-39-120.ec2.internal>
2023-02-24 20:25:55 +0000

    MiprimerCommit

---
+++ encontrar_numero.py
@@ -0,0 +1,10 @@
+def encontrar_numero_menor(arreglo):
+    if len(arreglo) == 0:
+        return None
+    menor = arreglo[0]
+    for numero in arreglo[1:]:
+        if numero<menor:
+            menor  = numero
+    return menor 
+    
+    
---
+++ test_encontrar_numero.py
@@ -0,0 +1,6 @@
+from encontrar_numero import encontrar_numero_menor
+
+def test_encontrar_menor():
+    assert encontrar_numero_menor([10,20,100,4])==4
+    
+    
